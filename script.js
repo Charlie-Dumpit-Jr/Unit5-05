742429284c73faa1c1f529cf342abe51ee1085b5
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-18 14:03:42 +0000

    Unit5-05: Calculating Area of Square(1)

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+//sets value of the number 1 and 2
 let num1 = 0
 let num2 = 0
 let answer = 0
 
+//detects when button is clicked and calculates the answer
 document.getElementById('results').addEventListener('click', solve)
 
+//solves the area
 function solve() {
-  
   num1 = document.getElementById('lenght').value
   num2 = document.getElementById('width').value
   answer = num1 * num2
